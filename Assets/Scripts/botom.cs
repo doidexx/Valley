e4cc603e80ad78d63a0a7fc4792d79569ec7f913
doidexx <doidexx000@gmail.com>
2018-10-30 23:30:11 -0400

    scenes added

---
+++ Assets/Scripts/botom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class botom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Next() {
+		SceneManager.LoadScene(4);
+	}
+	
+	public void StartGame(){
+		SceneManager.LoadScene(1);
+	}
+
+	public void PlayAgain(){
+		SceneManager.LoadScene(4);
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+}
